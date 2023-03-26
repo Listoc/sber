fbcf44a5bce585ae57c5fedaf0935281469077d9
Ivan Belomestnov <mestis0408@gmail.com>
2023-03-26 17:32:31 +0300

    Add two types of sorting
    Sorting by Name
    Sorting by District

--- src/Listok/sber/Main.java
+++ src/Listok/sber/Main.java
@@ -2,6 +2,7 @@ package Listok.sber;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -36,10 +37,30 @@ public class Main {
         }
     }
 
+    public static void sortByName(List<City> listOfCities) {
+        listOfCities.sort((city1, city2) -> city1.getName().compareToIgnoreCase(city2.getName()));
+    }
+
+    public static void sortByDistrict(List<City> listOfCities) {
+
+        listOfCities.sort(new Comparator<City>() {
+            @Override
+            public int compare(City city1, City city2) {
+                if(city1.getDistrict().equals(city2.getDistrict())) return city1.getName().compareTo(city2.getName());
+                return city1.getDistrict().compareTo(city2.getDistrict());
+            }
+        });
+
+    }
+
     public static void main(String[] args) {
         var listOfCities = new ArrayList<City>();
         try {
             readCities(listOfCities, "citiesList.csv");
+            sortByName(listOfCities);
+            printCities(listOfCities);
+            for(int i = 0; i < 100; ++i) System.out.println("-".repeat(100));
+            sortByDistrict(listOfCities);
             printCities(listOfCities);
         } catch (IOException e) {
             System.out.println("Problems with file!");
